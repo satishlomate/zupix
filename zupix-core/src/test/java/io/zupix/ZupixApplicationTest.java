282f9660f722c8f59eacaaa08c2f8203ae521594
Satish Lomate <satishlomate308@gmail.com>
2026-08-16 02:29:37 +0530

    test: update application creation test for current API

--- zupix-core/src/test/java/io/zupix/ZupixApplicationTest.java
+++ zupix-core/src/test/java/io/zupix/ZupixApplicationTest.java
@@ -2,12 +2,25 @@ package io.zupix;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ZupixApplicationTest {
 
     @Test
-    void createsApplication() {
-        assertNotNull(ZupixApplication.create());
+    void createsApplication() throws Exception {
+        var application = new TestApi();
+        try (var zupix = ZupixApplication.create(application, 0)) {
+            assertNotNull(zupix);
+            assertEquals(1, zupix.router().routes().size());
+            assertEquals(0, zupix.port());
+        }
+    }
+
+    static final class TestApi {
+        @Get("/hello")
+        String hello() {
+            return "hello";
+        }
     }
 }
