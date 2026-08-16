1b4230567bead5bae18cb5b348b5b41af3be13b1
Satish Lomate <satishlomate308@gmail.com>
2026-08-16 15:48:39 +0530

    test: cover structured JSON response flow

---
+++ zupix-core/src/test/java/io/zupix/JsonResponseIntegrationTest.java
@@ -0,0 +1,18 @@
+package io.zupix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class JsonResponseIntegrationTest {
+    @Test
+    void responseCanCarryStructuredJsonBody() {
+        record User(String name, int age) {}
+        var response = Response.ok(new User("Satish", 30));
+
+        assertEquals(200, response.status());
+        assertEquals("Satish", ((User) response.body()).name());
+        assertEquals(30, ((User) response.body()).age());
+        assertEquals("Satish", Json.read(Json.write(response.body()), User.class).name());
+    }
+}
