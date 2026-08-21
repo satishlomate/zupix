092a1b4d616647916671b4b1e621c5d82433d06d
Satish Lomate <satishlomate308@gmail.com>
2026-08-21 23:42:56 +0530

    cleanup: use existing Authenticator contract

--- zupix-core/src/main/java/io/zupix/security/AuthenticationProvider.java
+++
@@ -1,9 +0,0 @@
-package io.zupix.security;
-
-import io.zupix.RequestContext;
-
-/** Resolves authentication for an incoming request. */
-@FunctionalInterface
-public interface AuthenticationProvider {
-    Authentication authenticate(RequestContext request);
-}
