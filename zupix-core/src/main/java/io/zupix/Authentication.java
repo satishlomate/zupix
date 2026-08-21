2591bb2c309e2691f256af64eb2e3a509f14e88e
Satish Lomate <satishlomate308@gmail.com>
2026-08-21 23:39:19 +0530

    cleanup: remove duplicate security abstraction

--- zupix-core/src/main/java/io/zupix/Authentication.java
+++
@@ -1,7 +0,0 @@
-package io.zupix;
-
-/** Resolves the authenticated principal for an HTTP request. */
-@FunctionalInterface
-public interface Authentication {
-    Principal authenticate(RequestContext request);
-}
